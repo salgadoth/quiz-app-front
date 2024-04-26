42c50d03e74e6e2747bb7ded4605744ac13ce632
Thiago <thiagobtw437@gmail.com>
2024-04-26 21:35:19 +1000

    Added user statistics to dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,7 @@
-import { Card, CardActions, CardContent, CardHeader, IconButton, Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Typography } from "@mui/material";
 import * as Auth from "../../utils/auth"
 import { useEffect, useState } from "react";
 import axios, { HttpStatusCode } from "axios";
-import { FavoriteBorder } from "@mui/icons-material";
 
 const Dashboard = () => {
     const [tokenClaims, setTokenClaims] = useState<any>()
@@ -10,7 +9,7 @@ const Dashboard = () => {
 
     const [totalAnswers, setTotalAnswers] = useState<any>([])
     const [prcAnswers, setPrcAnswers] = useState<Object>({})
-    const [recentAnswers, setRecentAnswers] = useState<any>({})
+    const [recentAnswers, setRecentAnswers] = useState<any>([{}])
 
     useEffect(() => {
         if(!Auth.isAuthenticated())
@@ -26,19 +25,19 @@ const Dashboard = () => {
                     const response = await axios.get("http://localhost:8000/api/user/total-answers", {headers: {"Authorization": "Bearer " + token}})
                     if(response.status === HttpStatusCode.Ok){
                         setTotalAnswers(response.data)
-                        console.log(response.data);
+                        console.log(totalAnswers);
                     }
 
                     const responseTwo = await axios.get("http://localhost:8000/api/user/prct-answers", {headers: {"Authorization": "Bearer " + token}})
                     if(responseTwo.status === HttpStatusCode.Ok){
                         setPrcAnswers(responseTwo.data)
-                        console.log(responseTwo.data);
+                        console.log(prcAnswers);
                     }
 
                     const responseThree = await axios.get("http://localhost:8000/api/user/recent-answers", {headers: {"Authorization": "Bearer " + token}})
                     if(responseThree.status === HttpStatusCode.Ok){
                         setRecentAnswers(responseThree.data)
-                        console.log(responseThree.data);
+                        console.log(recentAnswers);
                     }
                 } catch (error) {
                     console.error('There was an error while trying to fetch for user related statistics.')
@@ -57,32 +56,37 @@ const Dashboard = () => {
                 <div>
                     <Typography variant="subtitle1">Welcome to your dasboard {tokenClaims.username}!</Typography>
                 </div>
-                <div>
+                <div style={{marginTop: '1em', marginBottom: '2em'}}>
                     <Typography variant="h5">Your results so far: </Typography>
                     <div>
                     <Card sx={{ maxWidth: 345, margin: '.5em' }}>
                         <CardHeader
+                            title={Object.keys(prcAnswers)}
                         />
                         <CardContent>
-                            <Typography variant="body2" color="text.secondary">
+                            <Typography variant="body2" color="text.secondary" style={{fontWeight: 'bold', fontSize: '1em', textAlign: 'center'}}>
+                                {Object.values(prcAnswers)}%
                             </Typography>
                         </CardContent>
-                        <CardActions disableSpacing>
-                            <IconButton aria-label="add to favorites">
-                            <FavoriteBorder/>
-                            </IconButton>
-                        </CardActions>
                     </Card>
                     </div>
                 </div>
                 <div>
                     <Typography variant="h5">Your recently answered questions: </Typography>
-                    <div>
-
+                    <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}}>
+                        {recentAnswers.map((ans: any, idx: any) => {
+                            return(<Card key={idx} sx={{ maxWidth: 345, margin: '.5em' }}>
+                            <CardHeader
+                                title={ans.quiz_title}
+                            />
+                            <CardContent>
+                                <Typography variant="body2" color="text.secondary" style={{fontWeight: 'bold', fontSize: '1em', textAlign: 'center'}}>
+                                    {new Date(ans.submited_at).toLocaleString('en-US', {weekday: 'long',year: 'numeric',month: 'long',day: 'numeric',hour: 'numeric',minute: 'numeric',second: 'numeric'})}
+                                </Typography>
+                            </CardContent>
+                        </Card>)
+                        })}
                     </div>
-                </div>
-                <div>
-                    <Typography variant="h5">Your completed quizzes: </Typography>
                 </div>
             </div>
         )
